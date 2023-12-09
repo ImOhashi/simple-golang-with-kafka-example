3936a52c530a24a57ca8d4e89638022f73d6c7b9
ImOhashi <leonardoohashi.mkt@gmail.com>
2023-12-09 17:00:44 -0300

    a sample test to create a kafka topic with golang

---
+++ create-topic.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+)
+
+func main() {
+	bootstrapServers := "http://localhost:9092"
+	topic := "topic-with-golang"
+	numParts := 2
+
+	admin, err := kafka.NewAdminClient(&kafka.ConfigMap{"bootstrap.servers": bootstrapServers})
+
+	if err != nil {
+		fmt.Printf("Failet to create admin client: %s\n", err)
+		os.Exit(1)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	maxDur, err := time.ParseDuration("60s")
+
+	if err != nil {
+		panic("ParseDuration(60s)")
+	}
+
+	results, err := admin.CreateTopics(
+		ctx,
+		[]kafka.TopicSpecification{{
+			Topic:         topic,
+			NumPartitions: numParts,
+		}},
+		kafka.SetAdminOperationTimeout(maxDur))
+
+	if err != nil {
+		fmt.Printf("Failed to create topic: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, result := range results {
+		fmt.Printf("%s\n", &result)
+	}
+
+	admin.Close()
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module simple-golang-with-kafka-example
+
+go 1.19
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20231202071711-9a357b53e9c9 // indirect
+	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
+	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.8.2 // indirect
+)
